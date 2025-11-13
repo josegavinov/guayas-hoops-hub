fb56e637a59b07761fb1081454c0bf6da1808901
Jose Andres Gaviño Villacis <jagavino@espol.edu.ec>
2025-11-13 04:57:35 +0000

    cambios del navbar y configuracion de entorno

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -14,16 +14,16 @@ const Navbar = () => {
   };
 
   return (
-    <nav className="fixed top-0 left-0 right-0 z-50 bg-background/95 backdrop-blur-sm border-b border-border">
-      <div className="container mx-auto px-4">
-        <div className="flex items-center justify-between h-20">
+    <nav className="fixed top-0 left-0 right-0 z-50 bg-background/95">
+      <div className="container ">
+        <div className="flex items-center justify-evenly h-20">
           <div className="text-2xl font-bold text-gradient">
             ABG
           </div>
 
           {/* Desktop Menu */}
-          <div className="hidden md:flex items-center gap-8">
-            <button onClick={() => scrollToSection("inicio")} className="text-foreground hover:text-primary transition-colors font-medium">
+          <div className="hidden md:flex items-strech gap-20">
+            <button onClick={() => scrollToSection("inicio")} className="text-foreground hover:text-primary transition-colors font-medium bold">
               Inicio
             </button>
             <button onClick={() => scrollToSection("nosotros")} className="text-foreground hover:text-primary transition-colors font-medium">
