9c3f9b7d674ec3e37a4bd4a299187c2e6bd0ab2f
Jose Andres Gaviño Villacis <jagavino@espol.edu.ec>
2025-11-14 03:50:52 +0000

    modificacion del .env y cambios en el firebase.ts

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -7,13 +7,13 @@ import { getFirestore } from "firebase/firestore";
 
 // Prefer using environment variables. If not set, se usan valores embebidos provistos.
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY || "AIzaSyBZwV-PQB_dFwTMX33J0hVJnpFN8EjBneQ",
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN || "mi-landing1.firebaseapp.com",
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID || "mi-landing1",
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET || "mi-landing1.firebasestorage.app",
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID || "126697889484",
-  appId: import.meta.env.VITE_FIREBASE_APP_ID || "1:126697889484:web:2f4a4328d29109f56c6c69",
-  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID || "G-1BBWC3NNMX",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN ,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID ,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID ,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID ,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID ,
 };
 
 // Initialize Firebase
