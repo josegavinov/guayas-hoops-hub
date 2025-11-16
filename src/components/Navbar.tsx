53ec2c56b251c20b38428c368f9abf1b40cb9fdb
Jose Andres Gaviño Villacis <jagavino@espol.edu.ec>
2025-11-16 02:28:58 +0000

    cambios en el navbar, daba problemas con otras dimensiones

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -16,7 +16,7 @@ const Navbar = () => {
   return (
     <nav className="fixed top-0 left-0 right-0 z-50 bg-background/95">
       <div className="container ">
-        <div className="flex items-center justify-evenly h-20">
+        <div className="flex items-center justify-between h-20">
           <div className="text-2xl font-bold text-gradient">
             ABG
           </div>
